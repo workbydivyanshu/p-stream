ffccfabff2b91cd4131615312964b1aa404429ae
workbydivyanshu <lifeofdivyu@proton.me>
2025-10-10 13:47:48 -0600

    remove unused translation on skip episode button

--- src/components/player/internals/SkipEpisodeButton.tsx
+++ src/components/player/internals/SkipEpisodeButton.tsx
@@ -1,5 +1,4 @@
 import { useCallback } from "react";
-import { useTranslation } from "react-i18next";
 
 import { Icons } from "@/components/Icon";
 import { usePlayerMeta } from "@/components/player/hooks/usePlayerMeta";
@@ -14,7 +13,6 @@ interface SkipEpisodeButtonProps {
 }
 
 export function SkipEpisodeButton(props: SkipEpisodeButtonProps) {
-  const { t } = useTranslation();
   const meta = usePlayerStore((s) => s.meta);
   const { setDirectMeta } = usePlayerMeta();
   const setShouldStartFromBeginning = usePlayerStore(
