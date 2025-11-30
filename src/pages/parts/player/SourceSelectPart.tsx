2ad0221bb6b76fc19466dd506324af64714493a7
workbydivyanshu <lifeofdivyu@proton.me>
2025-11-30 16:30:15 -0700

    fix source selection part not respecting disabled sources

--- src/pages/parts/player/SourceSelectPart.tsx
+++ src/pages/parts/player/SourceSelectPart.tsx
@@ -135,13 +135,15 @@ export function SourceSelectPart(props: { media: ScrapeMedia }) {
   const enableLastSuccessfulSource = usePreferencesStore(
     (s) => s.enableLastSuccessfulSource,
   );
+  const disabledSources = usePreferencesStore((s) => s.disabledSources);
 
   const sources = useMemo(() => {
     const metaType = props.media.type;
     if (!metaType) return [];
     const allSources = getCachedMetadata()
       .filter((v) => v.type === "source")
-      .filter((v) => v.mediaTypes?.includes(metaType));
+      .filter((v) => v.mediaTypes?.includes(metaType))
+      .filter((v) => !disabledSources.includes(v.id));
 
     if (!enableSourceOrder || preferredSourceOrder.length === 0) {
       // Even without custom source order, prioritize last successful source if enabled
@@ -189,6 +191,7 @@ export function SourceSelectPart(props: { media: ScrapeMedia }) {
     props.media.type,
     preferredSourceOrder,
     enableSourceOrder,
+    disabledSources,
     lastSuccessfulSource,
     enableLastSuccessfulSource,
   ]);
