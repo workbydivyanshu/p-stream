8d793baa9a6c6e289217aa8c16919b5cc059a6bf
workbydivyanshu <lifeofdivyu@proton.me>
2026-02-20 17:44:43 -0700

    attempt to fix captions visibility issue
    some users need to enable hardware acceleration for the to show

--- src/components/player/base/SubtitleView.tsx
+++ src/components/player/base/SubtitleView.tsx
@@ -167,11 +167,12 @@ export function SubtitleView(props: { controlsShown: boolean }) {
   return (
     <Transition animation="slide-up" show>
       <div
-        className="text-white absolute w-full flex flex-col items-center transition-[bottom]"
+        className="pointer-events-none z-50 text-white absolute w-full flex flex-col items-center transition-[bottom]"
         style={{
           bottom: props.controlsShown
             ? "6rem"
             : `${styling.verticalPosition}rem`,
+          transform: "translateZ(0)",
         }}
       >
         <SubtitleRenderer />
